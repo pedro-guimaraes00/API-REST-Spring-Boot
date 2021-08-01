fca5fdaaec42a4a4cc00749ac315625324095922
Pedro Guimarães <pedrogms654@gmail.com>
2021-08-01 18:17:37 -0300

    Adiciona anotação @PageableDefault no projeto

--- produtos/src/main/java/com/estoque/api/produtos/ProdutosApplication.java
+++ produtos/src/main/java/com/estoque/api/produtos/ProdutosApplication.java
@@ -2,8 +2,10 @@ package com.estoque.api.produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ProdutosApplication {
 
 	public static void main(String[] args) {
--- produtos/src/main/java/com/estoque/api/produtos/resource/ProdutoResource.java
+++ produtos/src/main/java/com/estoque/api/produtos/resource/ProdutoResource.java
@@ -2,8 +2,9 @@
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estoque.api.produtos.model.Produto;
@@ -32,10 +32,9 @@ public class ProdutoResource {
 	
 	@GetMapping("/produtos")
 	@ApiOperation(value="Retorna lista de produtos")
-	public Page<Produto> listaProdutos(@RequestParam int page, @RequestParam int qnt) {
+	public Page<Produto> listaProdutos(@PageableDefault(sort= "id", direction = Direction.DESC) Pageable pageable) {
 		
-		Pageable paginacao = PageRequest.of(page, qnt);
-		return produtoRepository.findAll(paginacao);
+		return produtoRepository.findAll(pageable);
 	}
 	
 	@GetMapping("/produtos/{id}")
